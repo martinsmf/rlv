1e86884bdc09f8222b9e79778229c9579e398f4c
Matheus F Martins <martinsmf00@gmail.com>
2021-04-13 00:21:17 -0300

    Iniciando automação da api
    Funcionalidade de login.

--- api/spec/routes/session.rb
+++ api/spec/routes/session.rb
@@ -2,8 +2,7 @@ class Sessions
     include HTTParty
     base_uri "http://rocklov-api:3333"
     
-    def login(email, pass)
-        payload = { email: email, password: pass}
+    def login(payload)
 
         return self.class.post(
             "/sessions",
