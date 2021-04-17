661421eec861dadefa8ee45adf273e90fca315c6
Matheus F Martins <martinsmf00@gmail.com>
2021-04-17 19:24:28 -0300

    Automatizando get equipos

---
+++ api/spec/get/get_equipo_spec.rb
@@ -0,0 +1,35 @@
+describe "GET /equipos/{equipo_id}"do
+
+    before(:all) do
+        payload = { email: "brook@onepiece.com", password: "teste123"}
+        result = Sessions.new.login(payload)
+        @user_id = result.parsed_response["_id"]
+    end
+
+    context "obter unico equipo" do
+        before(:all)do
+            @massa_dados = {
+                thumbnail: Helpers::get_thumb("sanfona.jpg"),
+                name: "Sanfona",
+                category: "Teclas",
+                price: 500
+            }
+
+            # cadastrando e pegando o id do equipamente, para garantir a massa de dados do teste
+            MongoDB.new.remove_equipo(@massa_dados[:name], @user_id)
+            equipo = Equipos.new.new_equipo(@massa_dados, @user_id)
+            @equipo_id = equipo.parsed_response["_id"]
+
+            # ralizando a requisição para get por id para buscar um unico equipamento.
+            @result = Equipos.new.find_by_id(@equipo_id, @user_id)
+        end
+
+        it "deve retornar 200" do
+            expect(@result.code).to eql 200
+        end
+
+        it "deve retornar o nome" do
+            expect(@result.parsed_response["name"]).to eql @massa_dados[:name]
+        end
+    end
+end
